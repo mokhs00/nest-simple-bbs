71af0f0ce59ad9ce972bb8667e8a32133a8d0b18
mokhs00 <mokhs00@naver.com>
2022-01-02 22:43:29 +0900

    add : createBoard() to BoardRepository

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,5 +1,19 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Board } from './entity/boards.entity';
+import { BoardStatus } from './model/board-status.enum';
+import { CreateBoardRequest } from './model/create-board.request';
 
 @EntityRepository(Board)
-export class BoardRepository extends Repository<Board> {}
+export class BoardRepository extends Repository<Board> {
+  async createBoard(createBoardRequest: CreateBoardRequest): Promise<Board> {
+    const { title, description } = createBoardRequest;
+
+    const board = this.create({
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    });
+
+    return this.save(board);
+  }
+}
