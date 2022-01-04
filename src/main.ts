017e9da59f072e5dfac5af065b12a6b4b6ef7080
mokhs00 <mokhs00@naver.com>
2022-01-05 07:55:14 +0900

    chore : 주석, log 제거

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,6 @@ async function bootstrap() {
 
   const port = serverConfig.port || 3000;
   await app.listen(port);
-
-  // config 모듈의 NODE_ENV default value는 development. local.EXT 가 있을 시 local이 마지막으로 로드되니 주의
-
-  logger.log(`profile : ${process.env.NODE_ENV || 'development'}`);
   logger.log(`Application running on port : ${port}`);
 }
 bootstrap();
