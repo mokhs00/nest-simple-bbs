71b422f7b8078285707e5aa9da1a5c9cec5f7e5b
mokhs00 <mokhs00@naver.com>
2022-01-05 06:11:28 +0900

    refactor : rename board -> boards

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BoardRepository } from './board.repository';
 import { BoardsController } from './boards.controller';
+import { BoardRepository } from './boards.repository';
 import { BoardsService } from './boards.service';
 
 @Module({
