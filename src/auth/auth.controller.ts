47fdbd89cf59cb267d8c5de51afe6aa905430162
mokhs00 <mokhs00@naver.com>
2022-01-05 06:03:23 +0900

    feat : login API

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { User } from './entity/user.entity';
 import { SignUpRequest } from './model/create-user.request';
+import { LoginRequest } from './model/login.request';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,12 @@ export class AuthController {
   @Post('/signup')
   signUp(@Body(ValidationPipe) signUpRequest: SignUpRequest): Promise<User> {
     return this.authService.signUp(signUpRequest);
+  }
+
+  @Post('/login')
+  login(
+    @Body(ValidationPipe) loginRequest: LoginRequest,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.login(loginRequest);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 import { User } from './entity/user.entity';
 import { SignUpRequest } from './model/create-user.request';
+import { LoginRequest } from './model/login.request';
 import { UserRepository } from './user.repository';
 
 @Injectable()
@@ -9,9 +16,31 @@ export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(signUpRequest: SignUpRequest): Promise<User> {
     return this.userRepository.signUp(signUpRequest);
+  }
+
+  async login(loginRequest: LoginRequest): Promise<{ accessToken: string }> {
+    const { username, password } = loginRequest;
+
+    const user = await this.userRepository.findOne({ username });
+
+    if (!user) {
+      throw new NotFoundException(
+        `Can't find User with username : ${username}`,
+      );
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('login failed');
+    }
+
+    const payload = { username };
+    const accessToken = await this.jwtService.sign(payload);
+
+    return { accessToken };
   }
 }
